f954644cd51a81ed5e53d74352a2839395acf9bd
Scott Caratozzolo <scaratozzolo12@gmail.com>
2017-12-28 11:20:56 -0500

    Moved everything to bioscan.py / optimazations

--- bioscan.py
+++ bioscan.py
@@ -5,28 +5,116 @@ Created on Dec 27, 2017
 
 I pledge my honor that I have abided by the Stevens Honor System - scaratoz
 '''
-from galleryscan import gallery_scan
-from userscan import user_scan
 import requests
 from bs4 import BeautifulSoup
 import time
 from datetime import datetime
 
 
+#subs = ["https://www.imgur.com","https://imgur.com/t/The_More_You_Know", "https://imgur.com/t/Science_and_Tech", "https://imgur.com/t/Gaming"]
+def gallery_scan():
+    print('Gallery scan started at: ' + str(datetime.now()))
+    
+    r = requests.get("https://www.imgur.com")
+    html_content = r.text
+    soup = BeautifulSoup(html_content, "html.parser")
+    
+    users = ""
+    gallery = ""
+    for link in soup.find_all('a'):
+        text = link.get('href')
+        if text.find("/user") > -1:
+            users += text + '\n'
+        elif text.find("/gallery") > -1 and len(text) <= 16:
+            if text == "/gallery/custom" or text == '/gallery/random':
+                pass
+            else:
+                gallery += text + '\n'      
+              
+    output_file = open('users.txt', 'a')
+    output_file.write(users)
+    output_file.close()
+    
+    output_file = open('gallery.txt', 'w')
+    output_file.write(gallery)
+    output_file.close()
+
+
+def user_scan():
+    print('User scan started at: ' + str(datetime.now()))
+    
+    input_file = open('gallery.txt', 'r')
+    
+    users = ""
+    for line in input_file:
+        imgur = "https://www.imgur.com" + line.strip()
+        r = requests.get(imgur)
+        html_content = r.text
+        soup = BeautifulSoup(html_content, "html.parser")
+        for link in soup.find_all('a'):
+            text = str(link.get('href'))
+            if text.find("//imgur.com/user") > -1:
+                if text.find("https://imgur.com") > -1:
+                    pass
+                else:
+                    users += text[11:] + '\n'  
+    
+    output_file = open('users.txt', 'a')
+    output_file.write(users)
+    output_file.close()
+
+    input_file.close()
+    repeat_users()
+
+
+def repeat_users():
+    print('Removing repeat users...')
+    
+    users = []
+    input_file = open('users.txt', 'r')
+    input_file2 = open('UsersDict.txt', 'r')
+    
+    for line in input_file:
+        users.append(line)
+        
+    for line2 in input_file2:
+        users.append(line2)
+        
+    users = set(users)
+    
+    input_file.close()
+    input_file2.close()
+    
+    output_file = open('UsersDict.txt', 'w')
+    
+    for user in users:
+        output_file.write(user)
+        
+    output_file.close()
+    
+    input_file = open('users.txt', 'w')
+    input_file.close()
+    
+    
+
 def bio_scan():
     print('Bio scan started at: ' + str(datetime.now()))
-    input_file = open('users.txt', 'r')
+    input_file = open('UsersDict.txt', 'r')
     
     bios = {}
+    parsed_bios = {}
     for user in input_file:
         imgur = 'https://www.imgur.com' + user.strip()
         r = requests.get(imgur)
         html_content = r.text
         soup = BeautifulSoup(html_content, "html.parser")
         
-        if soup.find('div', {'id' : 'account-bio'}) == None:
+        if user.strip()[6:] in parsed_bios:
             pass
+        elif soup.find('div', {'id' : 'account-bio'}) == None:
+            parsed_bios[user.strip()[6:]] = 'Parsed'
         else:
+            parsed_bios[user.strip()[6:]] = 'Parsed'
             bios[user.strip()[6:]] = soup.find('div', {'id' : 'account-bio'}).text
     
     for user in bios:
@@ -45,3 +133,4 @@ for i in range(1,101):
     bio_scan()
     print('Finished scan ' + str(i) + ' at: ' + str(datetime.now()) + '\n')
 print('Scanning finished at: ' + str(datetime.now()))
+
